a1f5af5f8c0b6f3cf3b21092f1376285051e0dbd
kimjihyo <kimjihyo0325@gmail.com>
2025-03-23 12:42:31 +0000

    add fonts and some configs

---
+++ src/config/site.ts
@@ -0,0 +1,12 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Jihyo Kim's Blog",
+  description: "Personal blog of Jihyo Kim, built with Next.js.",
+  url: "https://blog.jihyo.kim",
+  ogImage: "https://blog.jihyo.kim/opengraph-image.png",
+  links: {
+    github: "https://github.com/kimjihyo/blog.jihyo.kim",
+    githubProfile: "https://github.com/kimjihyo",
+  },
+};
