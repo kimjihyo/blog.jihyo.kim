841cacf30afbd28614d0906ca4fb731cdffe7f95
kimjihyo <kimjihyo0325@gmail.com>
2025-03-29 11:56:30 +0000

    change description of the og

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
   name: "Blog",
-  description: "Personal blog of Jihyo Kim, built with Next.js.",
+  description: "김지효씨의 개인 블로그",
   url: "https://blog.jihyo.kim",
   ogImage: "https://blog.jihyo.kim/opengraph-image.png",
   links: {
