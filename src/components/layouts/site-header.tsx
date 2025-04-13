eaa7719c56852a6b67edb3ea0079f82f6e7d79ab
kimjihyo <kimjihyo0325@gmail.com>
2025-04-13 18:41:58 +0900

    make title in siteeader more clickable

--- src/components/layouts/site-header.tsx
+++ src/components/layouts/site-header.tsx
@@ -1,21 +1,28 @@
 import { siteConfig } from "@/config/site";
 import Link from "next/link";
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import { ModeToggle } from "./mode-toggle";
 import { Shell } from "../shell";
+import { cn } from "@/lib/utils";
 
 export function SiteHeader() {
   return (
     <header className="sticky top-0 z-50 w-full h-16 border-b bg-background">
       <Shell className="py-0 h-full flex items-center gap-6">
-        <Link href="/" className="flex items-center space-x-2">
+        <Link
+          href="/"
+          className={cn(
+            buttonVariants({ variant: "ghost", size: "sm" }),
+            "flex items-center space-x-2"
+          )}
+        >
           <span className="font-extrabold">{siteConfig.name}</span>
         </Link>
         <div className="flex flex-1 items-center justify-end space-x-2">
-          <Button variant="primary" size="sm" asChild>
+          <Button size="sm" asChild>
             <Link href={siteConfig.links.github}>GitHub</Link>
           </Button>
-          <ModeToggle variant="default" />
+          <ModeToggle variant="muted" />
         </div>
       </Shell>
     </header>
