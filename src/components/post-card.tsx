9a6d8097a08a321e05a46f95a68cc3f99e5f6218
kimjihyo <kimjihyo0325@gmail.com>
2025-05-01 12:00:38 +0000

    blog posting and styling fix

--- src/components/post-card.tsx
+++ src/components/post-card.tsx
@@ -25,42 +25,41 @@ interface PostCardProps {
 export function PostCard({ post }: PostCardProps) {
   return (
     <article>
-      <Link
-        href={`/posts/${post._meta.path}`}
-        className="group h-full flex gap-5"
-      >
-        <div className="flex-1">
-          <div className="flex items-center gap-1 mb-1.5">
-            {post.tags.map((tag) => (
-              <Badge key={tag}>{tag}</Badge>
-            ))}
-          </div>
-          <h2 className="text-lg sm:text-xl font-semibold mb-1.5 text-foreground group-hover:text-primary transition-colors">
-            {post.title}
-          </h2>
-          <p className="text-muted-foreground text-sm sm:text-base">
-            {post.summary}
-          </p>
+      <Link href={`/posts/${post._meta.path}`} className="group h-full">
+        <div className="flex items-center gap-1 mb-1.5">
+          {post.tags.map((tag) => (
+            <Badge key={tag}>{tag}</Badge>
+          ))}
         </div>
-        <div>
-          <div className="w-[90px] h-[65px] sm:w-[130px] sm:h-[90px] mb-auto overflow-hidden relative">
-            {post.thumbnail ? (
-              <Image
-                fill
-                sizes="(min-width) 160px, 256px"
-                alt=""
-                className="object-center object-cover rounded bg-card"
-                src={post.thumbnail!}
-              />
-            ) : (
-              <div className="w-full h-full rounded bg-card" />
-            )}
-            <div className="absolute left-0 top-0 z-10 bg-background flex flex-col items-center justify-center w-full h-full -translate-y-full group-hover:translate-y-0 transition-transform duration-300">
-              <div className="text-2xl sm:text-4xl font-extrabold">
-                {post.createdTime.getDate()}
-              </div>
-              <div className="text-sm sm:text-base">
-                {months[post.createdTime.getMonth()]}
+        <div className="flex gap-5">
+          <div className="flex-1">
+            <h2 className="text-base sm:text-xl font-semibold mb-1.5 text-foreground group-hover:text-primary transition-colors">
+              {post.title}
+            </h2>
+            <p className="text-muted-foreground text-xs sm:text-base">
+              {post.summary}
+            </p>
+          </div>
+          <div>
+            <div className="w-[90px] h-[65px] sm:w-[130px] sm:h-[90px] overflow-hidden relative">
+              {post.thumbnail ? (
+                <Image
+                  fill
+                  sizes="(min-width) 160px, 256px"
+                  alt=""
+                  className="object-center object-cover rounded bg-card"
+                  src={post.thumbnail!}
+                />
+              ) : (
+                <div className="w-full h-full rounded bg-card" />
+              )}
+              <div className="absolute left-0 top-0 z-10 bg-background flex flex-col items-center justify-center w-full h-full -translate-y-full group-hover:translate-y-0 transition-transform duration-300">
+                <div className="text-2xl sm:text-4xl font-extrabold">
+                  {post.createdTime.getDate()}
+                </div>
+                <div className="text-sm sm:text-base">
+                  {months[post.createdTime.getMonth()]}
+                </div>
               </div>
             </div>
           </div>
