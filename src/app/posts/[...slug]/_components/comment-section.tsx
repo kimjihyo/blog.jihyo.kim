6fe596478c19826044eb0e96bb45c3645c0d4840
kimjihyo <kimjihyo0325@gmail.com>
2025-06-18 16:33:40 +0900

    remove unwanted shadow on the /subscribe-complete page

--- src/app/posts/[...slug]/_components/comment-section.tsx
+++ src/app/posts/[...slug]/_components/comment-section.tsx
@@ -18,7 +18,7 @@ export async function CommentSection({ postSlug }: CommentSectionProps) {
     .orderBy(desc(commentsTable.createdAt));
 
   return (
-    <div>
+    <div className="animate-fadeIn">
       <div className="flex items-center justify-between gap-2 mb-4">
         <div className="font-medium">댓글 {comments.length}</div>
         <div className="text-sm text-muted-foreground">
