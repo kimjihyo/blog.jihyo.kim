41503f004ed2772fd5f195f00e7faa35c8e51eee
kimjihyo <kimjihyo0325@gmail.com>
2025-07-01 16:00:32 +0900

    update title and description of the page

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
-  name: "blog.jihyo.kim",
-  description: "김지효씨의 개인 블로그",
+  name: "김지효의 기록 보관소",
+  description: "개발, 사담, 회고 등 개인적인 생각을 정리합니다.",
   url: "https://blog.jihyo.kim",
   avatarImage:
     "https://res.cloudinary.com/dab33vdij/image/upload/v1743915076/IMG_0420_wqr311.jpg",
