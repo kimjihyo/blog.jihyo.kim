ad7715ae95f537d469fbeefdec46c052f1402afc
kimjihyo <kimjihyo0325@gmail.com>
2025-08-21 14:08:51 +0900

    태그 바꿀 때 페이지 전체 리로드가 일어나는 이슈 해결

--- src/app/(home)/_components/tags.tsx
+++ src/app/(home)/_components/tags.tsx
@@ -47,17 +47,24 @@ export function Tags({ tags }: TagsProps) {
                   } else {
                     newTags.push(tag.name);
                   }
+                  setSelectedTags(newTags);
+
                   const params = new URLSearchParams(searchParams);
                   params.delete("tag");
                   params.delete("page");
                   newTags.forEach((t) => params.append("tag", t));
-                  setSelectedTags(newTags);
+
+                  // tag가 아예 없으면 uri가 '/'으로 되고, 이걸로 router.push하게 되면
+                  // 전체 페이지 리로드가 일어나는 이슈가 있었음.
+                  // 따라서 parmas가 아무것도 없는 것을 방지하기 위해 일단 임시 해결책
+                  if (newTags.length === 0) {
+                    params.set("tag", "");
+                  }
+
                   const queryString = params.toString();
                   const pathname = "/";
                   startTransition(() => {
-                    router.push(
-                      queryString ? `${pathname}?${queryString}` : pathname
-                    );
+                    router.push(`${pathname}?${queryString}`);
                   });
                 }}
               >
