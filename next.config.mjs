8fa41676e90943c2a1c718f7437b5abbb979d282
kimjihyo <kimjihyo0325@gmail.com>
2025-11-27 19:10:44 +0900

    chore: htmlLimitedBots을 삭제함

--- next.config.mjs
+++ next.config.mjs
@@ -10,9 +10,6 @@ const withBundleAnalyzer = createNextBundleAnalyzer({
  */
 const nextConfig = {
   cacheComponents: true,
-  /* config options here */
-  /* metadata 가 스트림돼서 body 태그에 들어가는 기능이 있는데. 원치않음. 아래 방식으로 비활성화 할 수 있음 참고: https://github.com/vercel/next.js/issues/793131 */
-  htmlLimitedBots: /.*/,
   serverExternalPackages: ["drizzle-orm"],
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   images: {
