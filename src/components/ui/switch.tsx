4ed836f2bc67c93e14eb6494bfccc5a27b851399
kimjihyo <kimjihyo0325@gmail.com>
2025-12-01 14:38:30 +0900

    feat: Switch 컴포넌트에서 framer-motion 사용을 제거하고 motion을 package.json 디펜던시에서도 제거함

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-import * as React from "react";
+import React from "react";
 import * as SwitchPrimitive from "@radix-ui/react-switch";
-
-import { motion } from "motion/react";
 
 import { cn } from "@/lib/utils";
 
@@ -40,29 +38,23 @@ function Switch({
     <SwitchPrimitive.Root
       data-slot="switch"
       className={cn(
-        "group peer inline-flex h-6 w-10 shrink-0 items-center rounded-full border border-transparent shadow-xs transition-all outline-none hover:border-primary focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:justify-end data-[state=checked]:bg-primary data-[state=unchecked]:bg-input dark:data-[state=unchecked]:bg-input/80",
+        "shadow-xs hover:border-primary focus-visible:border-ring focus-visible:ring-ring/50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input dark:data-[state=unchecked]:bg-input/80 group peer inline-flex h-6 w-10 shrink-0 items-center rounded-full border border-transparent outline-none transition-all focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
         className,
       )}
       checked={checked}
       onCheckedChange={onCheckedChange}
       {...props}
     >
-      <motion.div layout layoutDependency={checked}>
-        <SwitchPrimitive.Thumb
-          data-slot="switch-thumb"
-          className={cn(
-            "pointer-events-none flex size-5 items-center justify-center rounded-full bg-background ring-0 dark:data-[state=checked]:bg-primary-foreground dark:data-[state=unchecked]:bg-foreground",
-            thumbClassName,
-          )}
-        >
-          <div className="group-data-[state=unchecked]:hidden">
-            {iconChecked}
-          </div>
-          <div className="group-data-[state=checked]:hidden">
-            {iconUnchecked}
-          </div>
-        </SwitchPrimitive.Thumb>
-      </motion.div>
+      <SwitchPrimitive.Thumb
+        data-slot="switch-thumb"
+        className={cn(
+          "bg-background dark:data-[state=checked]:bg-primary-foreground dark:data-[state=unchecked]:bg-foreground ease-in-out-material transition-transform! pointer-events-none flex size-5 items-center justify-center rounded-full ring-0 duration-300 data-[state=checked]:translate-x-[calc(100%-2px)] data-[state=unchecked]:translate-x-0",
+          thumbClassName,
+        )}
+      >
+        <div className="group-data-[state=unchecked]:hidden">{iconChecked}</div>
+        <div className="group-data-[state=checked]:hidden">{iconUnchecked}</div>
+      </SwitchPrimitive.Thumb>
     </SwitchPrimitive.Root>
   );
 }
